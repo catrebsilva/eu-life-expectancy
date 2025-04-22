19b112194eb06363d87d783804f0653425241af4
Catarina Rebelo Da Silva <catrebsilva@olspbita063.hadoop01.corporativo.pt>
2025-04-22 12:36:27 +0100

    Remove linha acidental com 'git status'

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,4 +1,6 @@
-"""Cleaning module for life expectancy data."""
+def load_data() -> pd.DataFrame:
+    file_path = Path(__file__).parent / "data" / "eu_life_expectancy_raw.tsv"
+    return pd.read_csv(file_path, sep="\t")"""Cleaning module for life expectancy data."""
 
 # pylint: disable=missing-function-docstring
 
@@ -7,8 +9,7 @@ import pandas as pd
 
 
 def load_data() -> pd.DataFrame:
-    file_path = Path(__file__).parent / "data" git status
-/ "eu_life_expectancy_raw.tsv"
+    file_path = Path(__file__).parent / "data" / "eu_life_expectancy_raw.tsv"
     return pd.read_csv(file_path, sep="\t")
 
 
