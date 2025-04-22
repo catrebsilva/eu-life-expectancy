61f4990e869da839364f5955791768da821faae2
Catarina Rebelo Da Silva <catrebsilva@olspbita063.hadoop01.corporativo.pt>
2025-04-22 12:41:22 +0100

    Corrige erro acidental no load_data

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,6 +1,4 @@
-def load_data() -> pd.DataFrame:
-    file_path = Path(__file__).parent / "data" / "eu_life_expectancy_raw.tsv"
-    return pd.read_csv(file_path, sep="\t")"""Cleaning module for life expectancy data."""
+"""Cleaning module for life expectancy data."""
 
 # pylint: disable=missing-function-docstring
 
@@ -66,3 +64,4 @@ if __name__ == "__main__":
     parser.add_argument("--region", type=str, default="PT", help="Country code to filter data")
     args = parser.parse_args()
     main(args.region)
+
