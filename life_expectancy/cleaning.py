29459a7b35f801f440322a0c0bfcb32464bb0ccc
Catarina Rebelo Da Silva <catrebsilva@olspbita063.hadoop01.corporativo.pt>
2025-04-22 12:19:47 +0100

    Atualiza cleaning.py com código correto

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 
 def load_data() -> pd.DataFrame:
-    file_path = Path(__file__).parent / "data" / "eu_life_expectancy_raw.tsv"
+    file_path = Path(__file__).parent / "data" git status
+/ "eu_life_expectancy_raw.tsv"
     return pd.read_csv(file_path, sep="\t")
 
 
